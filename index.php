33ba9189c52e52feb0938965705afcb0f4fe6b50
baptuDenjer <niklas.imbiri@yahoo.com>
2022-07-28 15:01:20 +0900

    Looping in PHP Programming

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Looping in PHP Programming Language</title>
+</head>
+
+<body>
+
+    <h1>For Loop in PHP</h1>
+    <hr>
+
+    <?php
+        // ========= For Loop ==========
+        // for(startVariabel; range, increment){}
+
+        for($i = 0; $i < 5; $i++){
+            echo "SaCode Papua PHP II Assignment ", $i+1 , "<br>";
+        }
+    ?>
+
+</body>
+
+</html>
